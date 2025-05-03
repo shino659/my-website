67fa5223940b3c90bf6867a49b38e01be14b4332
shino659 <yg6razdmh@gmail.com>
2025-05-03 20:13:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
+// ページが読み込まれたときアラートを出す
+window.addEventListener('DOMContentLoaded', () => {
     alert('ページを見てくれてありがとう！');
+});
+
+// ボタンを押したらテキストを変える
+document.getElementById('changeTextBtn').addEventListener('click', () => {
+    document.getElementById('greeting').textContent = 'ありがとう！また会いましょう！';
 });
